259f65b16c025106734b1250d38dd7cdc5f8007c
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-10-21 12:30:50 +0530

    Add files via upload

---
+++ 21.10.2022/Investment.java
@@ -0,0 +1,25 @@
+package assignment;
+/* Write a Java method to compute the future investment value at a given interest rate for a specified number of years.
+
+Sample data (Monthly compounded):
+Input the investment amount: 1000
+Input the rate of interest: 10
+Input number of years:5 
+ */
+import java.util.Scanner;
+
+public class Investment {
+		 public static void main(String args[])
+		    {
+		        Scanner sc = new Scanner(System.in);
+		       
+		       System.out.print("Enter present value: ");
+			double p=sc.nextInt();
+			 System.out.print("Enter the interest rate: ");
+			 double r=sc.nextInt();
+			System.out.print("Enter the time period in years: ");
+			 double y=sc.nextInt();
+			double f=p*Math.pow((1+r/100),y);
+			System.out.print("value is: "+f);
+		  
+		}}
